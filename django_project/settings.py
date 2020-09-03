47fdec3a0f64bc2b4cfea21bb87c9da5551b3c26
unknown <carlos.alonsogtz@hotmail.es>
2020-09-03 20:23:15 +0200

    Set DEBUG value

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY =  os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG_VALUE = 'True'
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['alonso-django-blog.herokuapp.com']
 
